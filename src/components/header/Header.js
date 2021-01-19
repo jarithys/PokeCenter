48917e40ac1c1d4cd475d4f739b023782403bdd1
jarithys <s115309@ap.be>
2021-01-19 16:27:50 +0100

    Added Header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,35 +1,42 @@
-import { Link } from "gatsby"
-import PropTypes from "prop-types"
 import React from "react"
+import { useStaticQuery, graphql, Link } from "gatsby"
+import PropTypes from "prop-types"
+import Menu from "./Menu"
+import { HeaderWrapper, Image } from "./headerStyles/headerStyles"
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </header>
-)
+const Header = ({ siteTitle }) => {
+  const {
+    logo,
+    wpcontent: { menuItems },
+  } = useStaticQuery(graphql`
+  query {
+    logo: file(relativePath: {eq: "PokeLogo.png"}) {
+      childImageSharp {
+        fixed(quality: 100, width: 50) {
+          ...GatsbyImageSharpFixed_withWebp
+        }
+      }
+    }
+    wpcontent {
+    menuItems {
+      edges {
+        node {
+          label
+          path
+        }
+      }
+    }
+  }
+  }
+`)
+  return <HeaderWrapper>
+    <Link to="/">
+      <Image alt="logo poke center" fixed={logo.childImageSharp.fixed} />
+    </Link>
+    <Menu menuItems={menuItems.edges} />
+  </HeaderWrapper>
+}
+
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
