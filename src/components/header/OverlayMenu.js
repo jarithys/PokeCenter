4bd357df0680e2d876043e01b8031722092e6bf1
jarithys <s115309@ap.be>
2021-01-24 23:04:58 +0100

    Mobile menu - not working yet

---
+++ src/components/header/OverlayMenu.js
@@ -0,0 +1,54 @@
+import React from 'react'
+import { useStaticQuery, graphql, Link } from "gatsby";
+import { OverlayWrapper, Image, CloseButton, MenuList } from "./headerStyles/headerStyles"
+
+const OverlayMenu = ({ handleOverlayMenu, menuOpen }) => {
+    const {
+        logo,
+        wpcontent: { menuItems },
+    } = useStaticQuery(graphql`
+      query {
+        logo: file(relativePath: {eq: "PokeLogo.png"}) {
+          childImageSharp {
+            fixed(quality: 100, width: 50) {
+              ...GatsbyImageSharpFixed_withWebp
+            }
+          }
+        }
+        wpcontent {
+        menuItems {
+          edges {
+            node {
+              label
+              path
+            }
+          }
+        }
+      }
+    }
+    `)
+
+    return (
+        <>
+            {menuOpen && (
+                <OverlayWrapper>
+                    <CloseButton onClick={handleOverlayMenu}>X</CloseButton>
+                    <Link to="/" style={{marginBottom: "1.5rem"}}>
+                        <Image alt="logo pokecenter" fixed={logo.childImageSharp.fixed} />
+                    </Link>
+                    <MenuList style={{flexDirection: "column"}}>
+                        {menuItems.edges.map(({node: item}, i) =>  (
+                            <li key={i}>
+                                <Link activeClassName="nav-active" to={item.path}>
+                                    {item.label}
+                                </Link>
+                            </li>
+                        ))}
+                    </MenuList>
+                </OverlayWrapper>
+            )}
+        </>
+    )
+}
+
+export default OverlayMenu
