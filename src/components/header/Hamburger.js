c3c1007e13ace08a9616a13bf3f938c6636975d7
jarithys <s115309@ap.be>
2021-01-25 11:16:10 +0100

    Mobile Menu - Fixed
    onclick => onClick

--- src/components/header/Hamburger.js
+++ src/components/header/Hamburger.js
@@ -6,7 +6,7 @@ import { GiHamburgerMenu } from "react-icons/gi"
 
 const Hamburger = ({handleOverlayMenu}) => {
     return (
-        <HamburgerButton onclick={handleOverlayMenu} tabIndex="0">
+        <HamburgerButton onClick={handleOverlayMenu} tabIndex="0">
             <GiHamburgerMenu style={{width: "3rem", height: "3rem"}} />
         </HamburgerButton>
     )
